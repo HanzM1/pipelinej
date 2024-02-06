1d6c346453a401a9c5c3df91f908bb2467897233
HanzM1 <hanzmedina13@gmail.com>
2024-02-06 07:57:52 -0500

    agaiiin

--- Hello.java
+++ Hello.java
@@ -3,11 +3,4 @@ public static Main {
     System.out.println("Hello World in Jenkins");
 }
 }
-//first try
-//second try here on GitHub
-//No trigger from Jenkins
-//2/1 - attempting today for trigger
-//success, last test before closing this out
-//testing scripts now to see if jenkins can also build with this code
-//It worked, now last thing to check if this commit changes. If picked up then its a done deal
-//Done
+//Check
