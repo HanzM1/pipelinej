081bf1cd11a2baeccadad1fd4d56c4067351d784
HanzM1 <157531573+HanzM1@users.noreply.github.com>
2024-02-07 07:40:31 -0500

    Jenkin test

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public static Main { 
     public static void main(String[] args) {
-    System.out.println("Hello Everyone This is a test for Jenkins");
+    System.out.println("Hello Everyone, This is a test for Jenkins");
 }
 }
 //Checking again & again
