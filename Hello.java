bdf9255382213fb2395810a219f15ac17768e565
HanzM1 <157531573+HanzM1@users.noreply.github.com>
2024-02-08 15:38:28 -0500

    trigger

--- Hello.java
+++ Hello.java
@@ -3,5 +3,3 @@ public static Main {
     System.out.println("Hello World in Jenkins");
 }
 }
-//Checking for trigger -> still no trigger from jenkins
-//The trigger is done when its the original script -> Changing up the script causes issue. Will need to research different approach
